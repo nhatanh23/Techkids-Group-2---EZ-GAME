a3efc7216c28de5d18d667e5526ab062fbf41cbd
Ankhanhvu <ank.vu11@gmail.com>
2017-03-17 00:06:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import random
 import time
 
-a = random.randint(0,10)
-b = random.randint(0,10)
-d = random.randint(0,20)
-c = a + b
+# a = random.randint(0,10)
+# b = random.randint(0,10)
+# d = random.randint(0,20)
+# c = a + b
 #print(a, b, c, d)
 
 def play_or_exit():
@@ -18,6 +18,11 @@ def play_or_exit():
             print("Choice again: ")
 
 while play_or_exit():
+    a = random.randint(0, 10)
+    b = random.randint(0, 10)
+    d = random.randint(0, 20)
+    c = a + b
+    diem = 0
 
     print(a, b, c, d)
 
@@ -25,9 +30,12 @@ while play_or_exit():
     click = input("True or False").upper()
     elapsed_time = time.time() - start_time
 
-    if d == c and (elapsed_time < 2) and (click == "t"):
-        print("win")
-    elif d != c and (elapsed_time < 2) and (click == "f"):
-        print("win")
+    if d == c and (elapsed_time < 5) and (click == "T"):
+        diem +=1
+        print(diem, "win")
+        if d == c and elapsed_time)
+    elif d != c and (elapsed_time < 5) and (click == "F"):
+        diem +=1
+        print(diem, "win")
     else:
         print("lose")
